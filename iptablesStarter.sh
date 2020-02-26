307cf6a5eeb0a33c5a7d08ff72aba98c9ada2f15
amalone341 <42788620+amalone341@users.noreply.github.com>
2020-02-26 18:18:20 -0500

    Update iptablesStarter.sh

--- iptablesStarter.sh
+++ iptablesStarter.sh
@@ -3,16 +3,16 @@
 #Set up some standard iptables rules
 #Run as root
 
-iptables -A INPUT -i lo -j ACCEPT
-iptables -A INPUT -p tcp --dport 22 -j ACCEPT
-iptables -A INPUT -p tcp --dport 80 -j ACCEPT
-iptables -I INPUT 1 -p tcp --dport 22 -i eth0 -m state --state NEW -m recent --set
-iptables -I INPUT 2 -p tcp --dport 22 -i eth0 -m state --state NEW -m recent --update --seconds 600 --hitcount 5 -j DROP
-iptables -A INPUT -j DROP
+sudo iptables -A INPUT -i lo -j ACCEPT
+sudo iptables -A INPUT -p tcp --dport 22 -j ACCEPT
+sudo iptables -A INPUT -p tcp --dport 80 -j ACCEPT
+sudo iptables -I INPUT 1 -p tcp --dport 22 -i eth0 -m state --state NEW -m recent --set
+sudo iptables -I INPUT 2 -p tcp --dport 22 -i eth0 -m state --state NEW -m recent --update --seconds 600 --hitcount 5 -j DROP
+sudo iptables -A INPUT -j DROP
 
-iptables -A OUTPUT -i lo -j ACCEPT
-iptables -A OUTPUT -p tcp --dport 22 -j ACCEPT
-iptables -A OUTPUT -p tcp --dport 80 -j ACCEPT
-iptables -A INPUT -j DROP
+sudo iptables -A OUTPUT -i lo -j ACCEPT
+sudo iptables -A OUTPUT -p tcp --dport 22 -j ACCEPT
+sudo iptables -A OUTPUT -p tcp --dport 80 -j ACCEPT
+sudo iptables -A INPUT -j DROP
 
-iptables -L
+sudo iptables -L
