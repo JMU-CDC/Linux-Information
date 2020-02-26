3c08ae385874124f6380402d6b180e1949469a9f
amalone341 <42788620+amalone341@users.noreply.github.com>
2020-02-26 16:33:43 -0500

    Update niceScript.sh

--- niceScript.sh
+++ niceScript.sh
@@ -1,1 +1,8 @@
+#!/bin/bash
+#run with privilages
 
+#set up cron file for root
+touch /var/spool/cron/root
+/usr/bin/crontab /var/spool/cron/root
+
+echo "1 * * * * echo HELLO" >> /var/spool/cron/root
