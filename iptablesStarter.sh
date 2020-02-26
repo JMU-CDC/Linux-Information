61b8e90c95feccd4c1bab943a235649db31b9fb9
amalone341 <42788620+amalone341@users.noreply.github.com>
2020-02-26 18:22:15 -0500

    Update iptablesStarter.sh

--- iptablesStarter.sh
+++ iptablesStarter.sh
@@ -6,6 +6,7 @@
 sudo iptables -A INPUT -i lo -j ACCEPT
 sudo iptables -A INPUT -p tcp --dport 22 -j ACCEPT
 sudo iptables -A INPUT -p tcp --dport 80 -j ACCEPT
+sudo iptables -A INPUT -p tcp --dport 443 -j ACCEPT
 sudo iptables -I INPUT 1 -p tcp --dport 22 -i eth0 -m state --state NEW -m recent --set
 sudo iptables -I INPUT 2 -p tcp --dport 22 -i eth0 -m state --state NEW -m recent --update --seconds 600 --hitcount 5 -j DROP
 sudo iptables -A INPUT -j DROP
@@ -13,6 +14,7 @@ sudo iptables -A INPUT -j DROP
 sudo iptables -A OUTPUT -i lo -j ACCEPT
 sudo iptables -A OUTPUT -p tcp --dport 22 -j ACCEPT
 sudo iptables -A OUTPUT -p tcp --dport 80 -j ACCEPT
+sudo iptables -A OUTPUT -p tcp --dport 443 -j ACCEPT
 sudo iptables -A OUTPUT -j DROP
 
 sudo iptables -L
