60f1d7bf6ec5a935c5162a7053e19166fe185959
amalone341 <42788620+amalone341@users.noreply.github.com>
2020-02-26 20:15:48 -0500

    Update iptablesStarter.sh

--- iptablesStarter.sh
+++ iptablesStarter.sh
@@ -7,6 +7,8 @@ sudo iptables -A INPUT -i lo -j ACCEPT
 sudo iptables -A INPUT -p tcp --dport 22 -j ACCEPT
 sudo iptables -A INPUT -p tcp --dport 80 -j ACCEPT
 sudo iptables -A INPUT -p tcp --dport 443 -j ACCEPT
+sudo iptables -A INPUT -p tcp --syn --dport 22 -m connlimit --connlimit-above 3 -j REJECT
+sudo iptables -A INPUT -p tcp --dport 80 -m limit --limit 20/s --limit-burst 200 -j ACCEPT
 sudo iptables -I INPUT 1 -p tcp --dport 22 -i eth0 -m state --state NEW -m recent --set
 sudo iptables -I INPUT 2 -p tcp --dport 22 -i eth0 -m state --state NEW -m recent --update --seconds 600 --hitcount 5 -j DROP
 sudo iptables -A INPUT -j DROP
